7d49c814e998aaa45ad23522b7416c9ebb81d1ac
AnthonyYan021 <90709409+AnthonyYan021@users.noreply.github.com>
2021-09-14 13:03:12 -0300

    Update script.py
    Adicionada a condição que limita o usuário a uma escolha na sala 6 e adicionado o "if" e o "elif" que cria a lógica de progressão.

--- script.py
+++ script.py
@@ -7,22 +7,31 @@ def Escolhercaminho():
   global salaAtual
   global contadorDeTentativas
   global perdeu
+  
+   if (contadorDeTentativas == 7):
+    print("Voce usou mais tentativas do que eram permitidas")
+    perdeu = True
 
-  print("Voce esta na sala {}".format(salaAtual))
-  print("Escolha seu caminho:")
-  print("[1] - Caminho Vermelho")
-  print("[2] - Caminho Preto")
-  caminhoEscolhido = int(input())
+    print("Voce esta na sala {}".format(SalaAtual))
+    print("Escolha seu caminho:")
+    print("[1] - Caminho Vermelho")
+    print("[2] - Caminho Preto")
+    caminhoEscolhido = int(input())
+    
+    if (SalaAtual == 6 and caminhoEscolhido == 1):
+        SalaAtual = 5
+        print("Nao eh possivel sair dessa sala por esse caminho")
+
+    if (caminhoEscolhido == 1):
+        SalaAtual+= 1
+    elif (caminhoEscolhido == 2):
+        SalaAtual += 2
+    else:
+        print("Erro nao existe esse caminho")
 
   contadorDeTentativas += 1
 
-  if (contadorDeTentativas == 7):
-    perdeu = True
-
-  
-
-while (salaAtual < 9 and not(perdeu)):
+while (salaAtual < 9):
   Escolhercaminho()
 else:
-  if perdeu: print("\n\nVoce usou mais tentativas do que podiam vc PERDEU! \n \n \n \n")
-  else:  print("\n\nVoce conseguiu sair da dungeon com apenas {} tentativas \n \n \n \n".format(contadorDeTentativas)) 
+  print("Voce conseguiu sair da dungeon com apenas {} tentativas".format(contadorDeTentativas))
